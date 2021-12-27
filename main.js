03583b63921aa05709049387c26c04b701d23497
Fred <higgins.fred.s@gmail.com>
2021-12-27 12:19:27 -0800

    Completed Assignment

--- main.js
+++ main.js
@@ -3,7 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", ready); //Lets you know when DOM is loaded
 
+function ready () {
+  console.log("DOM is ready"); //Callback function for DOMContentIsLoaded
+}
+
+//Hides modal on page load
+let modal = document.getElementById("modal");
+modal.classList.add("hidden"); //assigns .hidden classname to modal
+
+
+
+
+//Sets up heart button
+let heartButton = document.querySelector(".like-glyph"); //assigns first class of .like-glyph to heartButton variable
+
+heartButton.innerHTML = EMPTY_HEART;
+
+//Triggers event when user presses heart button
+heartButton.addEventListener('click', function () {
+  mimicServerCall
+  
+  if (heartButton.innerHTML === EMPTY_HEART){
+    mimicServerCall().then((message) => {
+      heartButton.innerHTML = FULL_HEART;
+      heartButton.classList.add("activated-heart")
+      }).catch((message) => {
+        displayErr();
+        setTimeout(() => {modal.classList.add("hidden")}, 3000)//Hides error after 3 seconds
+      })
+  } else {
+    mimicServerCall().then((message) => {
+      heartButton.innerHTML = EMPTY_HEART;
+      heartButton.classList.remove("activated-heart")
+      }).catch((message) => {
+        displayErr();
+        setTimeout(() => {modal.classList.add("hidden")}, 3000)//Hides error after 3 seconds
+      })
+  }
+
+});
+
+//Displays Error
+function displayErr() {
+  modal.classList.remove("hidden")
+}
 
 
 
